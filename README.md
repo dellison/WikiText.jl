f9c231d8f602ab1345f3bf22ffc5e1575df12643
David Ellison <davidehellison@gmail.com>
2018-07-21 00:16:30 -0700

    initial commit for WikiText

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# WikiText
+# WikiText.jl
 
 [![Build Status](https://travis-ci.org/dellison/WikiText.jl.svg?branch=master)](https://travis-ci.org/dellison/WikiText.jl)
 
 [![Coverage Status](https://coveralls.io/repos/dellison/WikiText.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/dellison/WikiText.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/dellison/WikiText.jl/coverage.svg?branch=master)](http://codecov.io/github/dellison/WikiText.jl?branch=master)
+
+## About
+
+This package provides an interface to the [WikiText Long Term Dependency Language Modeling dataset](https://einstein.ai/research/the-wikitext-long-term-dependency-language-modeling-dataset)
+
+## Usage
+
+WikiText exports 6 things: 4 Types (WikiText2v1, WikiText103v1,
+WikiText2RawV1, WikiText103RawV1) and 3 functions (trainfile,
+validationfile, testfile).
+
+Downloading the datasets will happen automatically (with your
+approval) when you access them for the first time, courtesy of
+[DataDeps.jl](https://github.com/oxinabox/DataDeps.jl).
+
+```julia
+
+	julia> using WikiText
+
+    julia> corpus = WikiText2v1()
+
+	julia> trainfile(corpus)
+	"/path/to/wiki.train.tokens"
+```
--- src/WikiText.jl
+++ src/WikiText.jl
@@ -1,5 +1,138 @@
 module WikiText
 
-# package code goes here
+export WikiText2v1, WikiText103v1, WikiText2RawV1, WikiText103RawV1
+export trainfile, validationfile, testfile
+
+using DataDeps
+
+abstract type WikiTextCorpus end
+
+function filename(w::WikiTextCorpus, set)
+    @assert set in [:train, :valid, :test]
+    filename = "wiki.$set.$(suffix(w))"
+    return joinpath(corpusdir(w), filename)
+end
+
+trainfile(w) = filename(w, :train)
+validationfile(w) = filename(w, :valid)
+testfile(w) = filename(w, :test)
+
+abstract type WikiTextWordCorpus <: WikiTextCorpus end
+abstract type WikiTextCharCorpus <: WikiTextCorpus end
+
+suffix(w::WikiTextWordCorpus) = "tokens"
+suffix(w::WikiTextCharCorpus) = "raw"
+
+struct WikiText2v1   <: WikiTextWordCorpus end
+struct WikiText103v1 <: WikiTextWordCorpus end
+
+corpusdir(w::WikiText2v1)   = datadep"WikiText-2-v1"
+corpusdir(w::WikiText103v1) = datadep"WikiText-103-v1"
+
+struct WikiText2RawV1   <: WikiTextCharCorpus end
+struct WikiText103RawV1 <: WikiTextCharCorpus end
+
+corpusdir(w::WikiText2RawV1)   = datadep"WikiText-2-raw-v1"
+corpusdir(w::WikiText103RawV1) = datadep"WikiText-103-raw-v1"
+
+
+function __init__()
+
+    # utility function
+    moveup(x) = mv(x, joinpath("..", x))
+
+    RegisterDataDep("WikiText-2-v1",
+                    """
+                    Dataset: WikiText-2 word level language modeling dataset
+                    Author: Stephen Merity
+                    License: CC-SA 3.0
+                    Website: https://einstein.ai/research/the-wikitext-long-term-dependency-language-modeling-dataset
+                    
+                    Wikitext-2 word level long term dependency language modeling dataset.
+
+                    """,
+                    "https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip",
+                    "92675f1d63015c1c8b51f1656a52d5bdbc33aafa60cc47a218a66e7ee817488c",
+                    post_fetch_method = function (zip)
+                        unpack(zip)
+                        pth(x) = joinpath("..", x)
+                        cd("wikitext-2") do
+                            moveup("wiki.train.tokens")
+                            moveup("wiki.test.tokens")
+                            moveup("wiki.valid.tokens")
+                        end
+                        rm("wikitext-2")
+                    end)
+                    
+
+
+    RegisterDataDep("WikiText-103-v1",
+                    """
+                    Dataset: WikiText-103 word level language modeling dataset
+                    Author: Stephen Merity
+                    License: CC-SA 3.0
+                    Website: https://einstein.ai/research/the-wikitext-long-term-dependency-language-modeling-dataset
+                    Size: 181 MB
+                    
+                    Wikitext-103 word level long term dependency language modeling dataset.
+
+                    """,
+                    "https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip",
+                    "242ba0f20b329cfdf1ccc61e9e9e5b59becf189db7f7a81cd2a0e2fc31539590",
+                    post_fetch_method = function (zip)
+                        unpack(zip)
+                        cd("wikitext-103") do
+                            moveup("wiki.train.tokens")
+                            moveup("wiki.valid.tokens")
+                            moveup("wiki.test.tokens")
+                        end
+                    end)
+
+
+    RegisterDataDep("WikiText-2-raw-v1",
+                    """
+                    Dataset: WikiText-2 raw character level language modeling dataset
+                    Author: Stephen Merity
+                    License: CC-SA 3.0
+                    Website: https://einstein.ai/research/the-wikitext-long-term-dependency-language-modeling-dataset
+                    
+                    Wikitext-2 raw character level long term dependency language modeling dataset.
+
+                    """,
+                    "https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-raw-v1.zip",
+                    "ef7edb566e3e2b2d31b29c1fdb0c89a4cc683597484c3dc2517919c615435a11",
+                    post_fetch_method = function (zip)
+                        unpack(zip)
+                        cd("wikitext-2-raw") do
+                            moveup("wiki.train.raw")
+                            moveup("wiki.valid.raw")
+                            moveup("wiki.test.raw")
+                        end
+                        rm("wikitext-2-raw")
+                    end)
+
+    
+    RegisterDataDep("WikiText-103-raw-v1",
+                    """
+                    Dataset: WikiText-2 word level language modeling dataset
+                    Author: Stephen Merity
+                    License: CC-SA 3.0
+                    Website: https://einstein.ai/research/the-wikitext-long-term-dependency-language-modeling-dataset
+                    
+                    Wikitext-2 word level long term dependency language modeling dataset.
+
+                    """,
+                    "https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-raw-v1.zip",
+                    "91c00ae287f0d699e18605c84afc9e45c192bc6b7797ff8837e5474655a33794",
+                                        post_fetch_method = function (zip)
+                        unpack(zip)
+                        cd("wikitext-103-raw") do
+                            moveup("wiki.train.raw")
+                            moveup("wiki.valid.raw")
+                            moveup("wiki.test.raw")
+                        end
+                        rm("wikitext-103-raw")
+                    end)
+end
 
 end # module
